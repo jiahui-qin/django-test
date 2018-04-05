30c3fd8dc4a00f4d74d26eab5b5013625aca048a
saber <feverqjh@outlook.com>
2018-04-05 19:27:02 +0800

    4.5大修改！

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -4,13 +4,14 @@ from django.contrib.auth import views as auth_views
 
 app_name = 'blog'
 urlpatterns = [
-    url(r'^accounts/login/$', views.login_user, name = 'login_user'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^sortindexmz/$', views.sortindexmz, name='sortindexmz'),
-    url(r'^sortindexrt/$', views.sortindexrt, name='sortindexrt'),
-    url(r'^index/$', views.index, name='index'),
-    url(r'^highsearch/$', views.highsearch, name='highsearch'),
-    url(r'^accounts/logout/$', views.logout_user, name = 'logout_user'),
-    url(r'^managedata/$', views.managedata, name = 'managedata'),
+    url(r'^index/$', views.index, name='index')
+    #url(r'^accounts/login/$', views.login_user, name = 'login_user'),
+    #url(r'^search/$', views.search, name='search'),
+    #url(r'^sortindexmz/$', views.sortindexmz, name='sortindexmz'),
+    #url(r'^sortindexrt/$', views.sortindexrt, name='sortindexrt'),
+    #url(r'^index/$', views.index, name='index'),
+    #url(r'^highsearch/$', views.highsearch, name='highsearch'),
+    #url(r'^accounts/logout/$', views.logout_user, name = 'logout_user'),
+    #url(r'^managedata/$', views.managedata, name = 'managedata'),
 
 ]
--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -4,101 +4,102 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect  
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import meta
+#from .models import meta
 from django.db.models import Q
 from django.utils import timezone
 
-@login_required(login_url='/accounts/login/')
+
 def index(request):
-    meta_list = meta.objects.all().order_by('-updatetime')
-    #return HttpResponseRedirect(reverse(NAME_OF_PROFILE_VIEW, args=[request.user.username]))
-    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
-    '''
-    我们首先把 HTTP 请求传了进去，
-    然后 render 根据第二个参数的值 blog/index.html 找到这个模板文件并读取模板中的内容。
-    之后 render 根据我们传入的 context 参数的值把模板中的变量替换为我们传递的变量的值，
-    {{ title }} 被替换成了 context 字典中 title 对应的值，
-    同理 {{ welcome }} 也被替换成相应的值。
-    '''
-
-
-def search(request):
-    q = request.GET.get('q')
-    error_msg = ''
-    if not q:
-        error_msg = "please input keyword"
-        return render(request, 'blog/index.html', {'error_msg':error_msg})
-
-    meta_list = meta.objects.filter(Q(metabolomics__icontains = q))
-    if not meta_list:
-        return render(request, 'blog/index.html',{'title':'my database','error_msg':'no result!','meta_list':meta_list})
-    return render(request, 'blog/index.html',{'title':'my database','error_msg':error_msg,'meta_list':meta_list})
-
-def highsearch(request):
-    lowmz = request.GET.get('lowmz')
-    lowrt = request.GET.get('lowrt')
-    highmz = request.GET.get('highmz')
-    highrt = request.GET.get('highrt')
-    if not lowmz:
-        lowmz = 0
-    if not highmz:
-        highmz = float('inf')
-    if not lowrt:
-        lowrt = 0
-    if not highrt:
-        highrt = float('inf')
-    error_msg = "no result!"
-    meta_list = meta.objects.filter(Q(mz__gte = lowmz) & Q(mz__lte = highmz) & Q(rt__gte = lowrt) & Q(rt__lte = highrt))
-    if not meta_list:
-        return render(request, 'blog/index.html',{'title':'my database','error_msg':error_msg,'meta_list':meta_list})
-    return render(request, 'blog/index.html',{'title':'my database','meta_list':meta_list})
-
-def sortindexmz(request):
-    meta_list = meta.objects.all().order_by('mz')
-    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
-
-def sortindexrt(request):
-    meta_list = meta.objects.all().order_by('rt')
-    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
-
-def login_user(request):
-    state = "Please log in below..."
-    username = password = ''
-    if request.POST:
-        username = request.POST['username']
-        password = request.POST['password']
-
-        user = authenticate(username=username, password=password)
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                state = "You're successfully logged in!"
-                return HttpResponseRedirect('/index/')
-            else:
-                state = "Your account is not active, please contact the site admin."
-        else:
-            state = "Your username and/or password were incorrect."
-    return render(request, 'blog/login.html', context = {'title':'my database','welcome':'metabolomics database', 'state':state} )
-
-def logout_user(request):
-  logout(request)
-  # Redirect to a success page.
-  return HttpResponseRedirect("/accounts/login/")
-
-
-def managedata(request):
-    render(request, 'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':'插入成功'})
-
-def ppp(request):
-    state = '插入错误'
-    metabolomics = request.GET.get('metabolomics')
-    rt = request.GET.get('rt')
-    mz = request.GET.get('mz')
-    try:
-        cur = meta(metabolomics = 'metabolomics', rt = 'rt', mz = 'mz',  updatetime = timezone.now())
-        cur.save
-        return render(request,'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':'插入成功'})
-    except:
-        return render(request,'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':state})
-
-
+    return render(request, 'blog/tets.html')
+#@login_required(login_url='/accounts/login/')
+#def index(request):
+#    meta_list = meta.objects.all().order_by('-updatetime')
+#    #return HttpResponseRedirect(reverse(NAME_OF_PROFILE_VIEW, args=[request.user.username]))
+#    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
+#    '''
+#    我们首先把 HTTP 请求传了进去，
+#    然后 render 根据第二个参数的值 blog/index.html 找到这个模板文件并读取模板中的内容。
+#    之后 render 根据我们传入的 context 参数的值把模板中的变量替换为我们传递的变量的值，
+#    {{ title }} 被替换成了 context 字典中 title 对应的值，
+#    同理 {{ welcome }} 也被替换成相应的值。
+#    '''
+#
+#
+#def search(request):
+#    q = request.GET.get('q')
+#    error_msg = ''
+#    if not q:
+#        error_msg = "please input keyword"
+#        return render(request, 'blog/index.html', {'error_msg':error_msg})
+#
+#    meta_list = meta.objects.filter(Q(metabolomics__icontains = q))
+#    if not meta_list:
+#        return render(request, 'blog/index.html',{'title':'my database','error_msg':'no result!','meta_list':meta_list})
+#    return render(request, 'blog/index.html',{'title':'my database','error_msg':error_msg,'meta_list':meta_list})
+#
+#def highsearch(request):
+#    lowmz = request.GET.get('lowmz')
+#    lowrt = request.GET.get('lowrt')
+#    highmz = request.GET.get('highmz')
+#    highrt = request.GET.get('highrt')
+#    if not lowmz:
+#        lowmz = 0
+#    if not highmz:
+#        highmz = float('inf')
+#    if not lowrt:
+#        lowrt = 0
+#    if not highrt:
+#        highrt = float('inf')
+#    error_msg = "no result!"
+#    meta_list = meta.objects.filter(Q(mz__gte = lowmz) & Q(mz__lte = highmz) & Q(rt__gte = lowrt) & Q(rt__lte = highrt))
+#    if not meta_list:
+#        return render(request, 'blog/index.html',{'title':'my database','error_msg':error_msg,'meta_list':meta_list})
+#    return render(request, 'blog/index.html',{'title':'my database','meta_list':meta_list})
+#
+#def sortindexmz(request):
+#    meta_list = meta.objects.all().order_by('mz')
+#    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
+#
+#def sortindexrt(request):
+#    meta_list = meta.objects.all().order_by('rt')
+#    return render(request, 'blog/index.html', context = {'title':'my database','welcome':'metabolomics database', 'meta_list' : meta_list} )
+#
+#def login_user(request):
+#    state = "Please log in below..."
+#    username = password = ''
+#    if request.POST:
+#        username = request.POST['username']
+#        password = request.POST['password']
+#
+#        user = authenticate(username=username, password=password)
+#        if user is not None:
+#            if user.is_active:
+#                login(request, user)
+#                state = "You're successfully logged in!"
+#                return HttpResponseRedirect('/index/')
+#            else:
+#                state = "Your account is not active, please contact the site admin."
+#        else:
+#            state = "Your username and/or password were incorrect."
+#    return render(request, 'blog/login.html', context = {'title':'my database','welcome':'metabolomics database', 'state':state} )
+#
+#def logout_user(request):
+#  logout(request)
+#  # Redirect to a success page.
+#  return HttpResponseRedirect("/accounts/login/")
+#
+#
+#def managedata(request):
+#    render(request, 'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':'插入成功'})
+#
+#def ppp(request):
+#    state = '插入错误'
+#    metabolomics = request.GET.get('metabolomics')
+#    rt = request.GET.get('rt')
+#    mz = request.GET.get('mz')
+#    try:
+#        cur = meta(metabolomics = 'metabolomics', rt = 'rt', mz = 'mz',  updatetime = timezone.now())
+#        cur.save
+#        return render(request,'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':'插入成功'})
+#    except:
+#        return render(request,'blog/managedata.html', context =  {'title':'my database','welcome':'metabolomics database', 'state':state})
